c34d21963336134e225879b5e56469c57ab5b375
Mateja Srejić <hi@mateja.me>
2021-03-21 11:36:15 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,8 @@ $(function() {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logo.svg");
         } else {
             $(".navbar-area").addClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logo-2.svg");
         }
     });
 
